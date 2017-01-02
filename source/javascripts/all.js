287b3ae4b2b70647e425123f3d2c335de191d016
Kacper Golinski <kacper.golinski@gmail.com>
2017-01-02 20:30:29 +0100

    animate headroom and add nabber logo

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,8 @@
-// This is where it all goes :)
+var myElement = document.querySelector('.navbar');
+var options = {
+  "offset": 40,
+  "tolerance": 5,
+};
+
+var headroom  = new Headroom(myElement, options);
+headroom.init();
